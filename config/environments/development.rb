6d8b09f4f2cbb873b6487180ac8f48165335efaa
Rob Williams <robwilliams42@gmail.com>
2014-02-06 16:26:39 -0500

    Can't use production mode with wrk and I am tired of trying to figure out why, so instead using development env for everything

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,13 +4,13 @@ RailsTester::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
@@ -19,9 +19,11 @@ RailsTester::Application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.log_level = :info
+  config.log_formatter = ::Logger::Formatter.new
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 end
